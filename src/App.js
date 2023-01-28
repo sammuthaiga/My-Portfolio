9bb132927e2cfa4a6a029073cd81eb965f47c560
Samuel <samuel.muthaiga@student.moringaschool.com>
2023-01-29 02:18:10 +0300

    Imported About.jsx component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Home from "./components/Home";
 import NavBar from "./components/NavBar";
 import SocialLinks from "./components/SocialLinks";
+import About from "./components/About";
 
 
 function App() {
@@ -8,7 +9,7 @@ function App() {
     <div>
       <NavBar />
       <Home />
-
+      <About />
 
       <SocialLinks />
     </div>
